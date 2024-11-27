c6ab9147a963a84b11f5cf9a133d3d6e57bbef3a
RowanYoussef <150041546+RowanYoussef@users.noreply.github.com>
2024-11-27 19:25:58 +0200

    expected minmax

---
+++ Algorithms/expectedMinmax.py
@@ -0,0 +1,81 @@
+import utils
+class ExpectedMinmax():
+    def __init__(self):
+        self.utils = utils.Utils()
+    
+    #function to maximize the score
+    def maximize(self, board, k):
+        #if node is termenal
+        if k == 0:
+            player1_score = self.utils.calculate_score(board , 1)
+            player2_score = self.utils.calculate_score(board , 2)
+            return player1_score - player2_score,None
+        #initialize utiliy
+        maxUtility = float('-inf')
+        maxCol = None
+        for col in range(7):
+            #check if the column is not full
+            if board[0][col] == 0:
+                utility , _ = self.chance(board, col, 1, k)
+                #maximize the value of the game
+                if utility > maxUtility:
+                    maxUtility = utility
+                    maxCol = col
+        return maxUtility , maxCol
+
+    #function to minimize the score
+    def minimize(self, board, k):
+        #if node is termenal
+        if k == 0:
+            player1_score = self.utils.calculate_score(board , 1)
+            player2_score = self.utils.calculate_score(board , 2)
+            return player1_score - player2_score,None
+        #initialize utiliy
+        minUtility = float('inf')
+        minCol = None
+        for col in range(7):
+            #check if the column is not full
+            if board[0][col] == 0:
+                utility , _ = self.chance(board, col, 2, k)
+                #minimize the value of the game
+                if utility < minUtility:
+                    minUtility = utility
+                    minCol = col
+        return minUtility , minCol
+
+    #for chance nodes
+    def chance(self, board, col, player, k):
+        correct_move = 0.6
+        wrong_move = 0.2
+        valid_columns = [col]
+        #insert the valid columns and calculate probabilities based on the number of valid columns
+        if col - 1 > 0 and col + 1 < 7:
+            valid_columns.append(col - 1)
+            valid_columns.append(col + 1)
+        elif col - 1 > 0:
+            wrong_move = 0.4
+            valid_columns.append(col - 1)
+        else:
+            wrong_move = 0.4
+            valid_columns.append(col + 1)
+        #initialize expected utility
+        expected_utility = 0
+        for c in valid_columns:
+            if board[0][c] == 0:
+                row = self.utils.get_the_valid_row(board, c)
+                board[row][c] = player
+                #calculate utility for all expected nodes
+                if player == 1:
+                    utility, _ = self.minimize(board, k - 1)
+                else:
+                    utility, _ = self.maximize(board, k - 1)
+                board[row][c] = 0
+                if c == col:
+                    expected_utility += correct_move * utility
+                else:
+                    expected_utility += wrong_move * utility
+        return expected_utility,None   
+
+    def expectedMinmax(self ,board , k):
+        _ , maxCol = self.maximize(board , k)
+        return maxCol
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import Algorithms.minmax
+import Algorithms.expectedMinmax
 board = [
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 2, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 2, 0, 0, 0],
     [0, 0, 2, 2, 0, 0, 0],
     [1, 1, 2, 2, 0, 0, 0],
 ]
 res = Algorithms.minmax.Minmax()
+res1 = Algorithms.expectedMinmax.ExpectedMinmax()
 print(res.minmax(board , 4))
+print(res1.expectedMinmax(board , 4))
